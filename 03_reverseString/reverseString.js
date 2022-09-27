4c0ec78206e44c6a6e71e514bebaa7b4c0b3d9cc
MarcusCalgar <paraszthybalazs@gmail.com>
2022-09-27 13:27:00 +0200

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let lengthOfString = string.length;
+    let newStringHolder = [];
+    while(lengthOfString >= 0){
+        newStringHolder.push(string.charAt(lengthOfString--));
+    }
+    return newStringHolder.join("");
 };
 
 // Do not edit below this line
