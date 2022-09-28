ee764193c97c9a3072ca9161c95603f495c30de5
MarcusCalgar <paraszthybalazs@gmail.com>
2022-09-28 09:39:49 +0200

    Add solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(oldArray, ...values) {
+    const newArray = [];
+    oldArray.forEach(item => {
+        values.forEach(value => {
+            if(item !== value){
 
+                // If the new array doesn't have the item already 
+                // (because it has been added at a previous value check)
+                // AND the item is NOT in the values to be removed array
+                // (even if it isn't equal to the currently checked value)
+                
+                if (!newArray.includes(item) && !values.includes(item)){
+                    newArray.push(item); 
+                } 
+            }
+        });        
+       });
+    return newArray;
 };
 
 // Do not edit below this line
