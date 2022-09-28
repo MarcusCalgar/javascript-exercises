bd0525d0d2b863dffe376aa04ffb67824dc64aaf
MarcusCalgar <paraszthybalazs@gmail.com>
2022-09-28 10:57:31 +0200

    Add solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(tempInFahrenheit) {
+  return parseFloat(
+    ((tempInFahrenheit-32)*(5/9)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempInCelsius) {
+  return parseFloat((tempInCelsius*(9/5)+32).toFixed(1));
 };
 
 // Do not edit below this line
