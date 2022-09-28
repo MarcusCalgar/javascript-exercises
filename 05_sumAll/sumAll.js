4ed957b5f8bf95f66efa2187bf6c441e8361b02d
MarcusCalgar <paraszthybalazs@gmail.com>
2022-09-28 09:51:52 +0200

    Add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let result = 'ERROR';
+    if((typeof num1 =='number' && num1 > 0) && (typeof num2 == 'number' && num2 > 0)){
+        result = 0;
+        if(num1 > num2){
+            for(num2; num2 <= num1; num2++){
+                result += num2;
+            }
+        } else {
+            for(num1; num1 <= num2; num1++){
+                result += num1;
+            }
+        }
+        return result;
+    }
+    return result;
 };
 
 // Do not edit below this line
